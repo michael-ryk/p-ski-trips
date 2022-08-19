755fc1563a9e723a4a40dcff2e42d0f2728dc670
MichaelR <spider747@gmail.com>
2022-08-19 21:05:52 +0300

    Add Dummy data

---
+++ src/DummyData/DUMMYLIST.js
@@ -0,0 +1,20 @@
+const DUMMYLIST = [
+  {
+    id: 'a1',
+    site: 'Mayrhofen',
+    image: '<image url>',
+    country: 'Austria',
+    price: 1999,
+    description: 'Beautifull place',
+  },
+  {
+    id: 'a2',
+    site: 'Val Thorens',
+    image: '<image url>',
+    country: 'French',
+    price: 1800,
+    description: 'Nice French alps',
+  },
+];
+
+export default DUMMYLIST;
